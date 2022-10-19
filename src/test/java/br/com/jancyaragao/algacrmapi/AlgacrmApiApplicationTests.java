918f176a0fc9df818868e4610a134142eceb7124
Jâncy Aragão <jancy.aragao@gmail.com>
2022-10-19 08:43:18 -0300

    Inicialização do projeto com todos os arquivos criados pelo Spring Initializr

---
+++ src/test/java/br/com/jancyaragao/algacrmapi/AlgacrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jancyaragao.algacrmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgacrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
